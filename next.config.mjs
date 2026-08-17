337fe0a88ad9716103515ea163ad57676dbce9c7
Jared Snipes <153330843+0Jsnipes@users.noreply.github.com>
2026-08-17 17:05:22 -0400

    Configure public hero asset

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { formats: ["image/avif", "image/webp"] }
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        pathname: "/0Jsnipes/carolina-hot-chicken-demo/main/public/**"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
